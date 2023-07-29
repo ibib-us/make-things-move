84319535f01a237e7d252f60ab5750f9dd6e6bca
bobthechemist <bobthechemist@gmail.com>
2023-07-29 12:22:27 -0400

    initial commit

--- MakeThingsMove.ino
+++ MakeThingsMove.ino
@@ -12,24 +12,13 @@
 #define INCLUDE_GAMEPAD_MODULE
 #include <DabbleESP32.h>
 
-// Audiotools 
-#include "AudioTools.h"
-#include "AudioCodecs/CodecMP3Helix.h"
-#include "AudioLibs/AudioSourceSPIFFS.h"
 
 
-// Audio setup
-const char *startFilePath="/sounds";
-const char *ext="mp3";
-
-AudioSourceSPIFFS source(startFilePath, ext);
-AnalogAudioStream sink;
-MP3DecoderHelix decoder;
-AudioPlayer player (source, sink, decoder);
+// Servo seems to crash the uC, not sure if this is a timing or power issue.
 
 // Servo defines for low-level operation
-#define COUNT_LOW 1800
-#define COUNT_HIGH 7800
+#define COUNT_LOW 2000
+#define COUNT_HIGH 7600
 #define TIMER_WIDTH 16
 #define SERVO_PIN 16
 #define SERVO_CHANNEL 1
@@ -38,20 +27,17 @@ int servoCount = 4800;
 // drive pins
 int drivePos = 17;
 int driveNeg = 21;
-#define FWD_CHANNEL 2
-#define REV_CHANNEL 3
+int driveSpd = 14;
+#define FWD_CHANNEL 3
+#define REV_CHANNEL 2
+#define SPD_CHANNEL 4
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);      // make sure your Serial Monitor is also set at this baud rate.
   Dabble.begin("MS1m");       //set bluetooth name of your device
-  AudioLogger::instance().begin(Serial, AudioLogger::Warning); // Audiotools logger level
 
-  // begin audio processing
-  auto cfg = sink.defaultConfig();
-  sink.begin(cfg);
-  player.begin();
-  player.setAutoNext(false);
+
 
   // Set up servo PWM, fixed at 50 hz
   ledcSetup(SERVO_CHANNEL,50,TIMER_WIDTH);
@@ -59,33 +45,38 @@ void setup() {
     
 
   // Set drive pins
-  //pinMode(drivePos,OUTPUT);
-  //pinMode(driveNeg,OUTPUT);
-  //digitalWrite(driveNeg, LOW);
-  //digitalWrite(drivePos, LOW);
+  // Tried to PWM the EN pin on the L293, but that doesn't seem to be working, reverting. Update: Problem was possibly having too low a PWM.
+//  pinMode(drivePos,OUTPUT);
+//  pinMode(driveNeg,OUTPUT);
+//  digitalWrite(driveNeg, LOW);
+//  digitalWrite(drivePos, LOW);
   ledcAttachPin(drivePos, FWD_CHANNEL);
   ledcSetup(FWD_CHANNEL,5000,8);
   ledcWrite(FWD_CHANNEL,0);
   ledcAttachPin(driveNeg, REV_CHANNEL);
   ledcSetup(REV_CHANNEL, 5000,8);
   ledcWrite(REV_CHANNEL,0);
+  ledcAttachPin(driveSpd, SPD_CHANNEL);
+  ledcSetup(SPD_CHANNEL,5000,8);
+  ledcWrite(SPD_CHANNEL,255);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-  player.copy(); // refresh audio
+
   Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
   Serial.print("KeyPressed: ");
   if (GamePad.isUpPressed())
   {
-    player.setIndex(0);
+
     Serial.print("Up");
   }
 
   if (GamePad.isDownPressed())
   {
-    int idx;
-    idx = random(6);
-    player.setIndex(idx);
+
     Serial.print("Down");
   }
 
@@ -118,20 +109,21 @@ void loop() {
   if (GamePad.isCrossPressed())
   {
     Serial.print("Cross");
-    //digitalWrite(drivePos, HIGH);
-    ledcWrite(FWD_CHANNEL,200);
-    //digitalWrite(driveNeg, LOW);
-    ledcWrite(REV_CHANNEL, 0);
+//    digitalWrite(drivePos, HIGH);
+//    digitalWrite(driveNeg, LOW);
+    ledcWrite(REV_CHANNEL, 150);
+    ledcWrite(FWD_CHANNEL, 0);
+    
   } else if (GamePad.isTrianglePressed()) {
     Serial.print("Triangle");
-    //digitalWrite(drivePos, LOW);
-    ledcWrite(FWD_CHANNEL,0);
-    //digitalWrite(driveNeg, HIGH);
-    ledcWrite(REV_CHANNEL, 200);
+//    digitalWrite(drivePos, LOW);
+//    digitalWrite(driveNeg, HIGH);
+    ledcWrite(REV_CHANNEL, 0);
+    ledcWrite(FWD_CHANNEL, 200); // 100 is too slow.
   } else {
-    //digitalWrite(drivePos, LOW);
-    ledcWrite(FWD_CHANNEL,0);
-    //digitalWrite(driveNeg, LOW);
+//    digitalWrite(drivePos, LOW);
+//    digitalWrite(driveNeg, LOW);
+    ledcWrite(FWD_CHANNEL, 0);
     ledcWrite(REV_CHANNEL, 0);
   } 
 
